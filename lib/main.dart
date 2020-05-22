d3f4b21faec94791cfc8f784c333c1d5d96838e6
Carl Stanley <carl.stanley@deliveroo.co.uk>
2020-05-23 00:04:24 +0100

    Using List and ListView for continuous scrolling

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'Widgets/random_words_list/random_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: const Text('Hello World'),
+        body: Center(
+          child: RandomWords(),
         ),
-      ),
+      ),  
     );
   }
 }
